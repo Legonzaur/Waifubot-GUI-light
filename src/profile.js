2ca0c584af4513025e65ef4f4c3efe29f021fa52
Legonzaur <34353603+Legonzaur@users.noreply.github.com>
2021-02-20 22:55:01 +0100

    Create profile.js

---
+++ src/profile.js
@@ -0,0 +1,86 @@
+const main = document.querySelector("body main");
+const list = document.getElementById("list");
+const loadingIndicator = document.getElementById("loadingIndicator");
+const parameters = getUrlVars();
+var amount = 50;
+//display parameter
+//options : grid or list
+if (parameters.display == "list") {
+	displayMode(1);
+} else if (parameters.display == "grid") {
+	displayMode(0);
+}
+//amount parameter
+//option : any positive number
+if (
+	parameters.amount &&
+	!isNaN(Number(parameters.amount)) &&
+	Number(parameters.amount) > 0
+) {
+	let amount = Number(parameters.amount);
+	setAmount(amount);
+}
+
+//ser user and get Waifus
+
+//fake inventory generation
+var inventory = [];
+
+for (let i = 0; i < 1001; i++) {
+	inventory.push({
+		id: 1,
+		name: "Spike Spiegel",
+		image:
+			"https://images-ext-1.discordapp.net/external/CyPhtEnShv6UVITwTeidxS3e3N8CGCzJm4FWKYzSIfg/https/s4.anilist.co/file/anilistcdn/character/large/b1-ChxaldmieFlQ.png",
+	});
+}
+
+//fill "current" select with options
+for (var i = 0; i * amount < inventory.length; i++) {
+	let option = document.createElement("option");
+	let text =
+		i * amount +
+		"-" +
+		((i + 1) * amount > inventory.length ? inventory.length : (i + 1) * amount);
+	option.setAttribute("value", text);
+	option.innerText = text;
+	document.getElementById("current").appendChild(option);
+}
+
+document.getElementById("current");
+
+function setAmount(amount) {
+	let selected = Array.from(document.getElementById("amount")).find(
+		(e) => e.value == amount
+	);
+	if (!selected) {
+		selected = document.createElement("option");
+		selected.setAttribute("value", amount);
+		selected.innerText = amount;
+		document.getElementById("amount").appendChild(selected);
+	}
+	selected.selected = true;
+}
+
+function emptyList() {
+	list.innerHTML = "";
+}
+function addToListCharacter({ id, name, image }) {
+	list.innerHTML += `<div class="character">
+          <img src="${image}">
+          <p>${id} : ${name}</p>
+        </div>`;
+}
+function displayMode(mode) {
+	mode == 1 ? (list.className = "") : (list.className = "small");
+}
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(
+		/[?&]+([^=&]+)=([^&]*)/gi,
+		function (m, key, value) {
+			vars[key] = value;
+		}
+	);
+	return vars;
+}
