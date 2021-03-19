60bdd0d170efefa43226c7aeee9431a2d2464647
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-03-19 22:23:17 +0100

    V2.0 : dynamic load

--- src/profile.js
+++ src/profile.js
@@ -1,83 +1,53 @@
 const main = document.querySelector("body main");
 const list = document.getElementById("list");
-const parameters = getUrlVars();
-var amount = 50;
-var current = 0;
-var inventory = [];
+const sortElement = document.getElementById("sort");
+const searchInput = document.getElementById("searchInput");
+const parameters = new URLSearchParams(window.location.search.substring(1))
+var amount = Number(parameters.get("amount")) || 50;
+var user = {};
+var inventoryToShow = [];
 var filteredInventory = [];
-var compareInventory = [];
-//display parameter
-//options : grid or list
-if (parameters.display == "list") {
-	displayMode(1);
-} else if (parameters.display == "grid") {
-	displayMode(0);
-}
-//amount parameter
-//option : any positive number
-if (
-	parameters.amount &&
-	!isNaN(Number(parameters.amount)) &&
-	Number(parameters.amount) > 0
-) {
-	amount = Number(parameters.amount);
-}
+
 //theme parameter
-//options : black or white
-if (parameters.theme == "black") {
-	setTheme(0);
-} else if (parameters.theme == "white") {
-	setTheme(1);
+//options : black, white or gray
+setTheme(parameters.get("theme") || "black")
+
+
+fetchUserData(parameters.get("user")).then(e => {
+	user = e;
+	console.log(parameters.get("sort") != "Date")
+	filteredInventory = filter(e.Waifus, parameters.get("filter") || "")
+	if (parameters.get("sort") != "Date") sort(filteredInventory, parameters.get("sort"));
+	inventoryToShow = [...filteredInventory];
+	while (document.body.scrollHeight < window.innerHeight && inventoryToShow.length != 0) {
+		batchAddCards(inventoryToShow);
+	}
+});
+
+
+function batchAddCards(waifuList) {
+	let cards = document.createDocumentFragment();
+	inventoryToShow.splice(0, amount).forEach(waifu => {
+		let card = document.createElement("div");
+		let link = document.createElement("a");
+		let img = document.createElement("img");
+		let waifuID = document.createElement("p");
+		let waifuName = document.createElement("p");
+		card.appendChild(link);
+		link.appendChild(img);
+		card.appendChild(waifuID);
+		card.appendChild(waifuName);
+		link.setAttribute("href", "https://anilist.co/character/" + waifu.ID);
+		img.setAttribute("src", waifu.Image);
+		waifuID.innerText = waifu.ID;
+		waifuName.innerText = waifu.Name;
+		card.className = "character small";
+		cards.appendChild(card);
+	});
+	list.appendChild(cards);
 }
 
-//fetch user and get Waifus
-(async () => {
-
-	if (parameters.compare) {
-		await fetchUserData(parameters.compare).then(function (user) {
-			compareInventory = user.Waifus;
-		})
-	}
-
-	fetchUserData(parameters.user).then(function (user) {
-		inventory = user.Waifus;
-
-		//filter parameter
-		//options : any string or positive int
-		if (parameters.filter) {
-			filteredInventory = filter(inventory, parameters.filter)
-			searchInput.value = parameters.filter
-		} else {
-			//shallow copy
-			filteredInventory = [...inventory];
-		}
-
-		//sort parameter
-		//options : any or ID or Name
-		if (parameters.sort == "ID" || parameters.sort == "Name") {
-			sort(filteredInventory, parameters.sort)
-			sortElement.value = parameters.sort;
-		}
-
-		//fill "current" select with options
-		setCustomAmount(amount);
-		populateNavMenu(filteredInventory, amount);
-		displayList(filteredInventory, amount, current);
-		if (filteredInventory.length > current + amount) {
-
-			nextElement.disabled = false;
-		}
-		amountElement.disabled = false;
-		currentElement.disabled = false;
-		previousElement.disabled = true;
-		searchInput.disabled = false;
-		searchButton.disabled = false;
-		sortElement.disabled = false;
-	});
-})()
-
-async function fetchUserData(id) {
-	console.log("https://waifubot.kar.wtf/user/" + id)
+function fetchUserData(id) {
 	return fetch("https://waifubot.kar.wtf/user/" + id, {
 		headers: {
 			"Content-Type": "text/plain",
@@ -88,84 +58,28 @@ async function fetchUserData(id) {
 			return response.json();
 		})
 }
-function emptyNavMenu() {
-	document.getElementById("current").innerHTML = "";
+
+function setTheme(theme) {
+	document.documentElement.className = theme;
 }
-function populateNavMenu(list, amount) {
-	for (var i = 0; i * amount < list.length; i++) {
-		let option = document.createElement("option");
-		let text =
-			i * amount +
-			"-" +
-			((i + 1) * amount > list.length ? list.length : (i + 1) * amount);
-		option.setAttribute("value", i * amount);
-		option.innerText = text;
-		document.getElementById("current").appendChild(option);
-	}
-}
-function displayList(list, amount, from) {
-	for (var i = 0; i < amount; i++) {
-		if (i + from < list.length) {
-			displayCharacter(list[i + from]);
-		}
-	}
-}
-function setCustomAmount(amount) {
-	let selected = Array.from(document.getElementById("amount")).find(
-		(e) => e.value == amount
-	);
-	if (!selected) {
-		selected = document.createElement("option");
-		selected.setAttribute("value", amount);
-		selected.innerText = amount;
-		document.getElementById("amount").appendChild(selected);
-	}
-	selected.selected = true;
-}
-function emptyList() {
-	list.innerHTML = "";
-}
-function displayCharacter({ ID, Name, Image }) {
-	let additionnalClass = "";
-	if (findCaracterInList({ ID, Name, Image }, compareInventory)) {
-		additionnalClass = "owned"
-	}
-	list.innerHTML += `<div class="character ${additionnalClass}">
-		  <a href="https://anilist.co/character/${ID}"><img src="${Image}"></a>
-          <p>${ID}</p>
-		  <p>${Name}</p>
-        </div>`;
-}
-function displayMode(mode) {
-	mode == 1 ? (list.className = "") : (list.className = "small");
-}
-function setTheme(mode) {
-	mode == 1 ? (document.documentElement.className = "") : (document.documentElement.className = "black")
-}
-function getUrlVars() {
-	var vars = {};
-	var parts = window.location.href.replace(
-		/[?&]+([^=&]+)=([^&]*)/gi,
-		function (m, key, value) {
-			vars[key] = value;
-		}
-	);
-	return vars;
-}
+
 function filter(list, input) {
+	searchInput.value = input
 	if (!isNaN(Number(input))) {
 		return list.filter(e => e.ID.toString().startsWith(input))
 	} else {
 		return list.filter(e => e.Name.toLowerCase().includes(input.toLowerCase()))
 	}
 }
+
 function sort(list, input) {
 	if (input == "ID") {
+		sortElement.value = input
 		list.sort((a, b) => a.ID - b.ID)
 	} else if (input == "Name") {
+		sortElement.value = input
 		list.sort((a, b) => a.Name.localeCompare(b.Name, 'fr', { ignorePunctuation: true }))
 	}
 }
-function findCaracterInList({ ID, Name, Image }, list) {
-	return list.find(e => e.ID == ID)
-}
+
+
