5b0c880ff0b87cf7a99adb6e0579271cbac08780
Karitham <plouis.pery@gmail.com>
2021-03-20 01:29:09 +0100

    a few code changes to clean the thing

--- src/interactive.js
+++ src/interactive.js
@@ -1,83 +1,81 @@
 let searchButton = document.getElementById("searchButton");
+let scrollLimit = document.getElementById("scrollLimit");
 const url = new URL(window.location);
 sortElement.disabled = false;
 searchButton.disabled = false;
 searchInput.disabled = false;
 
-
-window.addEventListener('resize', positionChange);
-document.addEventListener('scroll', positionChange);
-
-function positionChange(e) {
-	if (document.getElementById("scrollLimit").getBoundingClientRect().top < window.innerHeight) {
-		console.log(window.scrollMaxY, Math.round(window.scrollY))
-		do {
-			batchAddCards(inventoryToShow);
-		}
-		while (window.scrollMaxY == Math.round(window.scrollY) && inventoryToShow.length != 0)
-	}
+function positionChange() {
+  if (
+    document.getElementById("scrollLimit").getBoundingClientRect().top <
+    window.innerHeight
+  ) {
+    while (window.scrollMaxY - window.scrollY < window.screen.height * 2) {
+      batchAddCards(inventoryToShow);
+    }
+  }
 }
 
+window.addEventListener("resize", positionChange);
+document.addEventListener("scroll", positionChange);
 
+sortElement.addEventListener("change", () => {
+  filteredInventory = filter(user.Waifus, searchInput.value);
+  filteredInventory = sort(filteredInventory, sortElement.value);
 
-sortElement.addEventListener("change", (e) => {
-	if (sortElement.value == "Date") {
-		filteredInventory = filter(user.Waifus, searchInput.value)
-	} else {
-		sort(filteredInventory, sortElement.value)
-	}
-	url.searchParams.set('sort', sortElement.value);
-	window.history.pushState({}, '', url);
-	reloadAll();
+  url.searchParams.set("sort", sortElement.value);
+  window.history.pushState({}, "", url);
+
+  reloadAll();
 });
 
-searchButton.addEventListener("click", (e) => {
-	if (searchButton.disabled) return;
-	filteredInventory = filter(user.Waifus, searchInput.value)
-	if (sortElement.value != "Date") {
-		sort(filteredInventory, sortElement.value)
-	}
-	url.searchParams.set('filter', searchInput.value);
-	window.history.pushState({}, '', url);
-	reloadAll();
+searchButton.addEventListener("click", () => {
+  if (searchButton.disabled) return;
+
+  filteredInventory = filter(user.Waifus, searchInput.value);
+  inventoryToShow = sort(filteredInventory, sortElement.value);
+
+  url.searchParams.set("filter", searchInput.value);
+  window.history.pushState({}, "", url);
+  reloadAll();
 });
 
-searchInput.addEventListener("keyup", e => {
-	if (e.key == "Enter") {
-		searchButton.click()
-	}
-})
+searchInput.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    searchButton.click();
+  }
+});
 
+window.addEventListener(
+  "popstate",
+  (e) => {
+    let targetParams = new URLSearchParams(
+      e.target.location.search.substring(1)
+    );
 
-window.addEventListener("popstate", e => {
-	let targetParams = new URLSearchParams(e.target.location.search.substring(1))
+    //filter parameter
+    //options : any string or positive int
+    if (targetParams.get("filter")) {
+      filteredInventory = filter(user.Waifus, targetParams.get("filter"));
+      searchInput.value = targetParams.get("filter");
+    } else {
+      //shallow copy
+      filteredInventory = [...user.Waifus];
+      searchInput.value = "";
+    }
 
-	//filter parameter
-	//options : any string or positive int
-	if (targetParams.get("filter")) {
-		filteredInventory = filter(user.Waifus, targetParams.get("filter"))
-		searchInput.value = targetParams.get("filter")
-	} else {
-		//shallow copy
-		filteredInventory = [...user.Waifus];
-		searchInput.value = "";
-	}
+    //sort parameter
+    //accepts any implemented by sort.
+    sortElement.value = targetParams.get("sort") ?? "Date";
+    sort(filteredInventory, sortElement.value);
 
-	//sort parameter
-	//options : any or ID or Name
-	if (targetParams.get("sort") == "ID" || targetParams.get("sort") == "Name") {
-		sort(filteredInventory, targetParams.get("sort"))
-		sortElement.value = targetParams.get("sort");
-	}
-
-	//amount parameter
-	amount = Number(targetParams.get("amount")) || amount;
-	reloadAll();
-}, false)
-
+    batchAddCards(inventoryToShow);
+  },
+  false
+);
 
 function reloadAll() {
-	list.innerHTML = "";
-	inventoryToShow = [...filteredInventory]
-	batchAddCards(inventoryToShow);
+  list.innerHTML = "";
+  inventoryToShow = [...filteredInventory];
+  batchAddCards(inventoryToShow);
 }
--- src/profile.js
+++ src/profile.js
@@ -2,84 +2,73 @@ const main = document.querySelector("body main");
 const list = document.getElementById("list");
 const sortElement = document.getElementById("sort");
 const searchInput = document.getElementById("searchInput");
-const parameters = new URLSearchParams(window.location.search.substring(1))
-var amount = Number(parameters.get("amount")) || 50;
+const parameters = new URLSearchParams(window.location.search.substring(1));
+var amount = 25;
 var user = {};
 var inventoryToShow = [];
 var filteredInventory = [];
 
 //theme parameter
 //options : black, white or gray
-setTheme(parameters.get("theme") || "black")
+document.documentElement.className = parameters.get("theme") ?? "black";
 
-
-fetchUserData(parameters.get("user")).then(e => {
-	user = e;
-	console.log(parameters.get("sort") != "Date")
-	filteredInventory = filter(e.Waifus, parameters.get("filter") || "")
-	if (parameters.get("sort") != "Date") sort(filteredInventory, parameters.get("sort"));
-	inventoryToShow = [...filteredInventory];
-	while (document.body.scrollHeight < window.innerHeight && inventoryToShow.length != 0) {
-		batchAddCards(inventoryToShow);
-	}
-});
-
+fetch("https://waifubot.kar.wtf/user/" + parameters.get("user")).then((r) =>
+  r.json().then((u) => {
+    user = u;
+    filteredInventory = filter(u.Waifus, parameters.get("filter") || "");
+    inventoryToShow = sort(filteredInventory, parameters.get("sort"));
+    batchAddCards(inventoryToShow);
+  })
+);
 
 function batchAddCards(waifuList) {
-	let cards = document.createDocumentFragment();
-	inventoryToShow.splice(0, amount).forEach(waifu => {
-		let card = document.createElement("div");
-		let link = document.createElement("a");
-		let img = document.createElement("img");
-		let waifuID = document.createElement("p");
-		let waifuName = document.createElement("p");
-		card.appendChild(link);
-		link.appendChild(img);
-		card.appendChild(waifuID);
-		card.appendChild(waifuName);
-		link.setAttribute("href", "https://anilist.co/character/" + waifu.ID);
-		img.setAttribute("src", waifu.Image);
-		waifuID.innerText = waifu.ID;
-		waifuName.innerText = waifu.Name;
-		card.className = "character small";
-		cards.appendChild(card);
-	});
-	list.appendChild(cards);
-}
+  let cards = document.createDocumentFragment();
 
-function fetchUserData(id) {
-	return fetch("https://waifubot.kar.wtf/user/" + id, {
-		headers: {
-			"Content-Type": "text/plain",
-		},
-		cache: "reload",
-	})
-		.then(function (response) {
-			return response.json();
-		})
-}
+  waifuList.splice(0, amount).forEach((waifu) => {
+    let card = document.createElement("div");
+    let link = document.createElement("a");
+    let img = document.createElement("img");
+    let waifuID = document.createElement("p");
+    let waifuName = document.createElement("p");
 
-function setTheme(theme) {
-	document.documentElement.className = theme;
+    card.appendChild(link);
+    link.appendChild(img);
+    card.appendChild(waifuID);
+    card.appendChild(waifuName);
+
+    link.setAttribute("href", "https://anilist.co/character/" + waifu.ID);
+    img.setAttribute("src", waifu.Image);
+    waifuID.innerText = waifu.ID;
+    waifuName.innerText = waifu.Name;
+    card.className = "character small";
+
+    cards.appendChild(card);
+    list.appendChild(cards);
+  });
 }
 
 function filter(list, input) {
-	searchInput.value = input
-	if (!isNaN(Number(input))) {
-		return list.filter(e => e.ID.toString().startsWith(input))
-	} else {
-		return list.filter(e => e.Name.toLowerCase().includes(input.toLowerCase()))
-	}
+  searchInput.value = input;
+
+  // if input is a number, filter using starts_with
+  var filt = !isNaN(Number(input))
+    ? (e) => e.ID.toString().startsWith(input)
+    : (e) => e.Name.toLowerCase().includes(input.toLowerCase());
+
+  return list.filter(filt);
 }
 
 function sort(list, input) {
-	if (input == "ID") {
-		sortElement.value = input
-		list.sort((a, b) => a.ID - b.ID)
-	} else if (input == "Name") {
-		sortElement.value = input
-		list.sort((a, b) => a.Name.localeCompare(b.Name, 'fr', { ignorePunctuation: true }))
-	}
+  sortElement.value = input;
+
+  switch (input) {
+    case "ID":
+      list.sort((a, b) => a.ID - b.ID);
+      break;
+    case "Name":
+      list.sort((a, b) =>
+        a.Name.localeCompare(b.Name, "fr", { ignorePunctuation: true })
+      );
+  }
+  return list;
 }
-
-
